5d157a7831ef9ef21d0afe5ba228d99182a94a92
Andre-Ovien <andreovien571@gmail.com>
2026-03-09 20:42:18 +0100

    service app for service flow-includes services offered alongside minor details

---
+++ backend/Core/Services/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .models import ServiceCategory, Service
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from .serializers import ServiceCategorySerializer, ServiceSerializer
+
+# Create your views here.
+
+class ServiceCategoryListCreateApiView(generics.ListCreateAPIView):
+    queryset = ServiceCategory.objects.all()
+    serializer_class = ServiceCategorySerializer
+    permission_classes = [IsAdminUser]
+
+
+class ServiceListCreateApiView(generics.ListCreateAPIView):
+    queryset = Service.objects.select_related('category').all()
+    serializer_class = ServiceSerializer
+    permission_classes = [IsAdminUser]
+
