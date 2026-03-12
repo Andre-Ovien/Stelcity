dab4a7caeb43a5cfde98c37d0a97b21f88481c58
Lah-deh <successasokere@gmail.com>
2026-03-12 11:07:45 +0100

    updated thhe home page

--- frontend/src/app/components/Main.js
+++ frontend/src/app/components/Main.js
@@ -1,8 +1,8 @@
-import Image from 'next/head'
+import Image from 'next/image'
 
 const Main = () => {
   return (
-    <div className="mx-3">
+    <div className="mx-3 ">
 
         <h1 className="text-[32px] font-bold ">Healthy <span className="text-[#C94F5C]">Skin</span><br/> Starts here</h1>
         <p className="text-[#826E70] text-[16px] font-normal mt-2">Indulge in a premium skincare experience crafted by experts, designed to transform your complexion and turn your daily routine into a moment of pure self-care.</p>
@@ -11,7 +11,24 @@ const Main = () => {
             <button className="bg-[#DAB9A8] text-[#B6454F] border-[#B6454F] rounded-3xl w-36 h-12">View Collection</button>
         </div>
 
-        <Image src="/images/lady.png" alt='lady' width={400} height={400} />
+        <Image src="/images/lady.png" alt='lady' width={400} height={400} className='mt-9' />
+        <div  className='flex items-center space-between gap-1.5 border-transparent shadow-lg rounded-2xl py-2 px-1' >
+
+            <div className='flex items-center'>
+                <Image  src="/images/tested.png" width={18} height={20} alt='tested' />
+                <p className='text-[9.5px]'>Dermatologist Tested</p>
+            </div>
+            <div className='flex items-center'>
+                <Image  src="/images/nature.png" width={18} height={20} alt='tested' />
+                <p  className='text-[9.5px]'>natural Ingredients</p>
+            </div>
+            <div className='flex items-center'>
+                <Image  src="/images/free.png" width={18} height={20} alt='tested' />
+                <p  className='text-[9.5px]'>Cruelty-free</p>
+            </div>
+        </div>
+
+        <h3 className='text-[#D65A5A] text-[14px] font-bold text-center mt-3 pb-3'>Trusted by 10,000+ happy customers.</h3>
     </div>
   )
 }
