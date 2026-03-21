ef1051fa351b000a2b28763ef9d45ff2d0f5ef96
Andre-Ovien <andreovien571@gmail.com>
2026-03-21 08:54:00 -0600

    new endpoints

--- backend/Core/Auth/urls.py
+++ backend/Core/Auth/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('login/',views.LoginView.as_view()),
     path('register/',views.RegisterView.as_view()),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('shipping-address/', views.ShippingAddressView.as_view(), name='shipping-address'),
 ]
