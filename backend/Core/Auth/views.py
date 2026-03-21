8cae0be855e6196a1402ec5a8084f911cc8cbb85
Andre-Ovien <andreovien571@gmail.com>
2026-03-21 08:55:03 -0600

    views for Profile and shipping address

--- backend/Core/Auth/views.py
+++ backend/Core/Auth/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .serializers import RegisterSerializer, LoginSerializer
+from .serializers import RegisterSerializer, LoginSerializer, ProfileSerializer, ShippingAddressSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.views import APIView
 from .throttles import LoginRateThrottle, RegisterRateThrottle
+from rest_framework.permissions import IsAuthenticated
+from .models import ShippingAddress
 
 # Create your views here.
 
@@ -53,3 +55,28 @@ class LoginView(APIView):
             },
             status=status.HTTP_200_OK
         )
+    
+class ProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
+        return self.request.user
+    
+class ShippingAddressView(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ShippingAddressSerializer
+
+    def get_object(self):
+        address, _ = ShippingAddress.objects.get_or_create(
+            user=self.request.user,
+            defaults={
+                'full_name': self.request.user.full_name or '',
+                'phone_number': self.request.user.phone_number or '',
+                'street_address': '',
+                'city': '',
+                'state': '',
+                'country': '',
+            }
+        )
+        return address
