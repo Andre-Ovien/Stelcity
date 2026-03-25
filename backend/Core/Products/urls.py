62ac04a67e4c36171b374f27d2dd22fbeeda5722
Andre-Ovien <andreovien571@gmail.com>
2026-03-25 13:26:13 -0600

    new endpoint

--- backend/Core/Products/urls.py
+++ backend/Core/Products/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('cart/checkout/', views.CartCheckoutView.as_view(), name='cart-checkout'),
     path('payment/webhook/', views.PaystackWebhookView.as_view(), name='paystack-webhook'),
     path('payment/verify/<str:reference>/', views.VerifyPaymentView.as_view(), name='verify-payment'),
+    path('delivery-fee/', views.DeliveryFeeView.as_view(), name='delivery-fee'),
 ]
