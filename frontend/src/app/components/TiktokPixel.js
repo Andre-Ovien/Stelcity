f1493445c0a500ce24047c944dbddb77ed8535a6
Andre-Ovien <andreovien571@gmail.com>
2026-08-04 10:57:33 +0100

    update

--- frontend/src/app/components/TiktokPixel.js
+++ frontend/src/app/components/TiktokPixel.js
@@ -11,6 +11,19 @@ export default function TikTokPixel() {
             w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
             ttq.load('D9IUH3JC77U7JAF0U2N0');
             ttq.page();
+            var pendingEvents=w.__stelcityTikTokEventQueue||[];
+            w.__stelcityTikTokEventQueue=[];
+            for(var q=0;q<pendingEvents.length;q++){
+              var pendingEvent=pendingEvents[q];
+              if(pendingEvent.eventId){
+                ttq.track(pendingEvent.eventName,pendingEvent.payload,{event_id:pendingEvent.eventId});
+              }else{
+                ttq.track(pendingEvent.eventName,pendingEvent.payload);
+              }
+              if(pendingEvent.onceKey){
+                try{w.localStorage.setItem('stelcity:tiktok:tracked:'+pendingEvent.onceKey,'1')}catch(e){}
+              }
+            }
           }(window, document, 'ttq');
         `,
       }}
